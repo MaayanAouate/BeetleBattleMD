bc6a6209fb1df89ba72afb6893a332bb1100a723
Twelve <Twelve@DESKTOP-MG3L518>
2025-10-23 11:02:24 +0300

    Add project files.

---
+++ Model/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+
+    public class Player
+    {
+        public int PlayerID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int IsAdmin { get; set; } // ערך 0 למשתמש וכל ערך אחר לאדמין
+        public byte[] ProfilePicture { get; set; }
+        public int Tokens { get; set; }
+        
+        public Player() { }
+        
+        public Player(int playerID, string userName, string password, string email, int isAdmin, byte[] profilePicture, int tokens)
+        {
+            PlayerID = playerID;
+            UserName = userName;
+            Password = password;
+            Email = email;
+            IsAdmin = isAdmin;
+            ProfilePicture = profilePicture;
+            Tokens = tokens;
+        }
+    }
+}
---
+++ Unitest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Unitest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
