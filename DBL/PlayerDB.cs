0daf181b6faa501fa2ebdbaae64a6f393646aa61
Twelve <Twelve@DESKTOP-MG3L518>
2025-10-26 09:42:35 +0200

    yes

--- DBL/PlayerDB.cs
+++ DBL/PlayerDB.cs
@@ -1,14 +1,39 @@
 ﻿using Model;
+using Mysqlx.Crud;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DBL
 {
-    public abstract class PlayerDB : BaseDB<Player>
+    public class PlayerDB : BaseDB<Player>
     {
-        
+        protected override async Task<Player> CreateModelAsync(object[] row)
+        {
+            Player bob = new Player();
+            bob.PlayerID = int.Parse(row[0].ToString());
+            bob.UserName = row[1].ToString();
+            bob.Email = row[3].ToString();
+            bob.IsAdmin = int.Parse(row[6].ToString());
+            bob.Tokens = int.Parse(row[5].ToString());
+            bob.ProfilePicture = (byte[])row[4];
+            return bob;
+        }
+
+        protected override string GetPrimaryKeyName()
+
+        {
+            return "PlayerID";
+        }
+
+        protected override string GetTableName()
+        {
+            return "player";
+        }
+
+        public async Insert
     }
 }
--- Unitest/Program.cs
+++ Unitest/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Unitest
+﻿using DBL;
+using Model;
+
+namespace Unitest
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, kabab romani hamodi!");
+            PlayerDB dB = new PlayerDB();
+
         }
     }
 }
