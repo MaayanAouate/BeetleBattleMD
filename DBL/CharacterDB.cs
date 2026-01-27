ab61996f50424a52cb262026b31e2076da1ac104
maayan awat <maayan.awat@gmail.com>
2026-01-27 15:55:12 +0200

    צריך לעשות db לדמויות

---
+++ DBL/CharacterDB.cs
@@ -0,0 +1,35 @@
+﻿using Model;
+using Mysqlx.Crud;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DBL
+{
+    public class CharacterDB : BaseDB<Character>
+    {
+        protected override async Task<Character> CreateModelAsync(object[] row)
+        {
+            Character beetle = new Character();
+            beetle.CharacterID = int.Parse(row[0].ToString());
+            beetle.CharacterName = row[1].ToString();
+            beetle.Attack = int.Parse(row[3].ToString());
+            beetle.HealthPoints = int.Parse(row[4].ToString());
+            beetle.Defence = int.Parse(row[5].ToString());
+            beetle.CritRate = double.Parse(row[6].ToString());
+
+            return beetle;
+        }
+
+        protected override string GetPrimaryKeyName()
+
+        {
+            return "CharacterID";
+        }
+
+    }
+}
---
+++ Model/Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Character
+    {
+        public int CharacterID { get; set; }
+        public string CharacterName { get; set; }
+        public int Attack { get; set; }
+        public int HealthPoints { get; set; }
+        public int Defence { get; set; }
+        public double CritRate { get; set; }
+    }
+}
